65ce0f83f851f4d2da89b25f78852a7d83e52b14
Allison <39247861+allison-abad@users.noreply.github.com>
2018-07-13 15:48:31 -0400

    Add files via upload

---
+++ coffeeApp/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://sandraattar:sandraattar1@ds131971.mlab.com:31971/househaydencoffeeapp' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
